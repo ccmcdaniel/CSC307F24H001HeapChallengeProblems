33c5c14324e4fbd6f0db948d91d1df21ea2960d8
Chad McDaniel <93947281+ccmcdaniel@users.noreply.github.com>
2024-11-13 12:10:56 -0600

    Implmented rest of heap operations.

--- Heap.h
+++ Heap.h
@@ -5,6 +5,11 @@
 using std::cout;
 using std::vector;
 
+//ToDo:
+//1. Complete Remove Operation ()
+//2. Heapify function
+//3. Add Array
+
 class Heap
 {
 private:
@@ -53,6 +58,61 @@ public:
 
 	}
 
+	bool GetNext(int & ret_item)
+	{
+		//Move last item in heap to root
+		//move the item down until the heap property is restored
+
+		if (heap.size() == 0)
+			return false;
+
+		else
+		{
+			if (heap.size() == 1)
+			{
+				ret_item = heap[0];
+				heap.erase(heap.begin());
+				return true;
+			}
+			else
+			{
+				//return the item back to where the function is called.
+				ret_item = heap[0];
+
+				//move the last item in the heap to the front.
+				heap[0] = heap[heap.size() - 1];
+
+				//delete the former position of the last item
+				heap.erase(heap.end() - 1);
+
+				//Restore the heap poperty on the root.
+				RestoreHeap(0);
+			}
+		}
+	}
+
+	vector<int> HeapSort()
+	{
+		vector<int> result;
+
+		int ret_item;
+		while(GetNext(ret_item))
+			result.push_back(ret_item);
+
+		return result;
+	}
+
+	void BuildHeap(vector<int> arr)
+	{
+		heap = arr;
+
+		int median = heap.size();
+		median = median / 2;
+
+		for (int i = median; i >= 0; i--)
+			RestoreHeap(i);
+	}
+
 	void PrintHeap()
 	{
 		for (int i = 0; i < heap.size(); i++)
@@ -77,5 +137,60 @@ private:
 		int temp = heap[a];
 		heap[a] = heap[b];
 		heap[b] = temp;
+	}
+
+	void RestoreHeap(int start_index)
+	{
+		bool done = false;
+		int curr_index = start_index;
+		do
+		{
+			int left_child_index = 2 * curr_index + 1;
+			int right_child_index = 2 * curr_index + 2;
+
+			//check if the item has childern  if not, we do not need
+			//to continue.
+			if (left_child_index >= heap.size())
+				done = true;
+
+			else if (right_child_index >= heap.size())
+			{
+				//look at the left side only
+				if (ChildHasPriority(left_child_index, curr_index))
+				{
+					swap(left_child_index, curr_index);
+					curr_index = left_child_index;
+				}
+				else
+					done = true;
+			}
+
+
+			else
+			{
+				//if left has priority, look at the left child
+				if (ChildHasPriority(left_child_index, right_child_index))
+				{
+					if (ChildHasPriority(left_child_index, curr_index))
+					{
+						swap(left_child_index, curr_index);
+						curr_index = left_child_index;
+					}
+					else
+						done = true;
+				}
+				//otherwise, the right child has priority over the left.
+				else
+				{
+					if (ChildHasPriority(right_child_index, curr_index))
+					{
+						swap(right_child_index, curr_index);
+						curr_index = right_child_index;
+					}
+					else
+						done = true;
+				}
+			}
+		} while (!done);
 	}
 };
--- testbench.cpp
+++ testbench.cpp
@@ -5,7 +5,7 @@ using std::cout;
 
 int main()
 {
-	Heap test_heap(false);
+	Heap test_heap;
 
 	test_heap.InsertItem(45);
 	test_heap.PrintHeap();
@@ -28,6 +28,16 @@ int main()
 	test_heap.InsertItem(5);
 	test_heap.PrintHeap();
 	cout << "\n";
+
+	vector<int> sorted_items = test_heap.HeapSort();
+
+	for (int i = 0; i < sorted_items.size(); i++)
+		cout << sorted_items[i] << ", ";
+
+	cout << "\n";
+
+	test_heap.BuildHeap({ 3, 89, 1, 12, 15, 17, 19, 88, 72, 100, 1, 2, 4 });
+	test_heap.PrintHeap();
 
 	//98, 78, 61, 22, 45, 56, 5
 }
